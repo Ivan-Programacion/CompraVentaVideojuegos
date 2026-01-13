e2089745d18c90f1f0a5194283c42a5d028b522d
Iván Vázquez Martín <ivan.vazquez@live.u-tad.com>
2026-01-13 13:01:19 +0100

    Commit inicial

---
+++ src/test/java/com/inicial/CompraVentaVideojuegosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inicial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompraVentaVideojuegosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
